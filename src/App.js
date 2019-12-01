4ebd808259233f3ba7e578fbf25ef033918af847
qubit88 <qubertec@gmail.com>
2019-12-02 00:27:43 +0200

    add routing, create store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import thunk from "redux-thunk";
+
+import currencyReducer from "./store/reducers/currencyReducer";
+import convertReducer from "./store/reducers/convertReducer";
+import RatesPage from "./pages/RatesPage";
+import ConvertPage from "./pages/ConvertPage";
+import Navigation from "./components/navigation/Navigation";
+import * as actions from "./store/actions";
+
+import "./App.css";
+
+const rootReducer = combineReducers({
+  rates: currencyReducer,
+  convert: convertReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 function App() {
+  useEffect(() => {
+    const timestampFromStorage = localStorage.getItem("timestamp");
+    const timestamp =
+      timestampFromStorage && Number(JSON.parse(timestampFromStorage));
+
+    if (timestamp && timestamp + 7200 * 1000 > new Date()) {
+      const ratesFromStorage = localStorage.getItem("rates");
+
+      let rates = JSON.parse(ratesFromStorage);
+      store.dispatch(actions.fetchRatesSuccess(rates));
+      console.log("storage");
+    } else {
+      store.dispatch(actions.fetchRates()).then(() => {
+        localStorage.setItem(
+          "rates",
+          JSON.stringify(store.getState().rates.rates)
+        );
+        localStorage.setItem("timestamp", JSON.stringify(+new Date()));
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navigation />
+          <Switch>
+            <Route path="/convert">
+              <ConvertPage />
+            </Route>
+            <Route path="/">
+              <RatesPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
